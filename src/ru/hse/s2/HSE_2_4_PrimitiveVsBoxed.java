a3df2f0c114de2655b2628fd43089547bbee78a1
Kirill Kosolapov <ctash@yandex-team.ru>
2020-01-21 02:36:51 +0300

    Add Primitive vs boxed. 128

--- src/ru/hse/s2/HSE_2_4_PrimitiveVsBoxed.java
+++ src/ru/hse/s2/HSE_2_4_PrimitiveVsBoxed.java
@@ -2,17 +2,17 @@ package ru.hse.s2;
 
 public class HSE_2_4_PrimitiveVsBoxed {
     public static void main(String[] args) {
-        Integer integerFromNew1 = new Integer(1);
-        Integer integerFromNew2 = new Integer(1);
-        Integer integerFromLitteral = 1;
-        Integer integerFromValueOf = Integer.valueOf(1);
+        Integer integerFromNew1 = new Integer(128);
+        Integer integerFromNew2 = new Integer(128);
+        Integer integerFromLitteral = 128;
+        Integer integerFromValueOf = Integer.valueOf(128);
 
         System.out.println(integerFromNew1 == integerFromNew2);
         System.out.println(integerFromNew2 == integerFromLitteral);
         System.out.println(integerFromLitteral == integerFromValueOf);
 
-        System.out.println(integerFromNew1 == 1);
+        System.out.println(integerFromNew1 == 128);
         System.out.println(integerFromNew1 == integerFromLitteral);
-        System.out.println(integerFromLitteral == 1);
+        System.out.println(integerFromLitteral == 128);
     }
 }

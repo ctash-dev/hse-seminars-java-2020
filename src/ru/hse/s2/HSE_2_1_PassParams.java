c503a9b0368e78d54e4a83f80025b707824884c3
Kirill Kosolapov <ctash@yandex-team.ru>
2020-01-21 02:11:11 +0300

    PassParams try3

--- src/ru/hse/s2/HSE_2_1_PassParams.java
+++ src/ru/hse/s2/HSE_2_1_PassParams.java
@@ -4,7 +4,7 @@ public class HSE_2_1_PassParams {
     public static void main(String[] args) {
         Counter counter = new Counter();
         incCounter(counter);
-        System.out.println(counter);
+        System.out.println(counter.value);
     }
 
     private static void incCounter(Counter counter) {

75b6362f90dee9e8f1fd406fa6b60acf6a1f1577
Kirill Kosolapov <ctash@yandex-team.ru>
2020-01-21 02:16:18 +0300

    AverageIntegers first try

--- src/ru/hse/s2/HSE_2_2_AverageIntegers.java
+++ src/ru/hse/s2/HSE_2_2_AverageIntegers.java
@@ -8,6 +8,6 @@ public class HSE_2_2_AverageIntegers {
                 Integer.MAX_VALUE - 2,
         };
         System.out.println(Integer.MAX_VALUE - 1);
-        System.out.println((numbers[0] + numbers[1] + numbers[2]) / 3);
+        System.out.println((numbers[0]/3 + numbers[1]/3 + numbers[2]/3));
     }
 }

a464779d07aceb4a9861d0b92b92a7af0709c8d8
Kirill Kosolapov <ctash@yandex-team.ru>
2020-01-21 02:26:39 +0300

    AverageDoubles try1

--- src/ru/hse/s2/HSE_2_3_AverageDoubles.java
+++ src/ru/hse/s2/HSE_2_3_AverageDoubles.java
@@ -4,7 +4,7 @@ public class HSE_2_3_AverageDoubles {
     public static void main(String[] args) {
         double[] numbers = {
                 Double.MAX_VALUE - 1,
-                Double.MIN_VALUE + 1
+                - Double.MAX_VALUE + 1
         };
         System.out.println((numbers[0] + numbers[1]) / 2);
         if (0.0 != (numbers[0] + numbers[1]) / 2) throw new AssertionError();

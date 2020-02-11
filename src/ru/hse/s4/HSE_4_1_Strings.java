12f5b561b32c9c587fa1ca1f01eb186bd0369612
Kirill Kosolapov <ctash@yandex-team.ru>
2020-02-11 09:18:03 +0300

    Strings toCharArray

--- src/ru/hse/s4/HSE_4_1_Strings.java
+++ src/ru/hse/s4/HSE_4_1_Strings.java
@@ -7,6 +7,12 @@ public class HSE_4_1_Strings {
         String secondS = firstS.toUpperCase();
         System.out.println(firstS.charAt(0));
         System.out.println(secondS.charAt(0));
+
+        char[] chars = firstS.toCharArray();
+        chars[0] = 'S';
+
+        System.out.println(firstS.charAt(0));
+        System.out.println(chars[0]);
     }
 
 }

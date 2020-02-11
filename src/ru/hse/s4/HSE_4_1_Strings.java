fb02e8430af69ec2d5dac80a4401744a25214867
Kirill Kosolapov <ctash@yandex-team.ru>
2020-02-11 09:17:07 +0300

    Strings toUpperCase immutable

---
+++ src/ru/hse/s4/HSE_4_1_Strings.java
@@ -0,0 +1,12 @@
+package ru.hse.s4;
+
+public class HSE_4_1_Strings {
+
+    public static void main(String[] args) {
+        String firstS = "string";
+        String secondS = firstS.toUpperCase();
+        System.out.println(firstS.charAt(0));
+        System.out.println(secondS.charAt(0));
+    }
+
+}

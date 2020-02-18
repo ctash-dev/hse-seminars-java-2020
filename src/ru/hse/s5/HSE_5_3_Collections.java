ec8901ca438624e767793db56aa8c725e52d3677
Kirill Kosolapov <ctash@yandex-team.ru>
2020-02-18 10:14:07 +0300

    Collections. Collection. Sort

---
+++ src/ru/hse/s5/HSE_5_3_Collections.java
@@ -0,0 +1,20 @@
+package ru.hse.s5;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class HSE_5_3_Collections {
+
+    public static void main(String[] args) {
+        List<Integer> list = new LinkedList<>();
+        Collection<Integer> collection = list;
+        list.addAll(List.of(1, 2, 3, 4, 5));
+        Collections.sort(list); // how it works?
+    }
+
+
+}

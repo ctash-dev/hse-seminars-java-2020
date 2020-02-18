919662941bd8162d0f221bef58f199d552d3066c
Kirill Kosolapov <ctash@yandex-team.ru>
2020-02-18 09:30:24 +0300

    Collections. Default structures.

---
+++ src/ru/hse/s5/Main.java
@@ -0,0 +1,36 @@
+package ru.hse.s5;
+
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        // array
+        int[] array = new int[1000];
+
+        // stack
+        Stack<Integer> stack = new Stack<>();
+
+        // queue
+        Queue<Integer> queue; // ?? queue is an interface
+
+        // list
+        List<Integer> list; // random access or linked?
+
+        // set
+        Set<Integer> set; // ?? set is an interface
+
+        // heap
+        // ???
+
+        // tree
+        // ???
+
+        // hashtable
+        Hashtable<Integer, Integer> hashtable = new Hashtable<>();
+
+    }
+}

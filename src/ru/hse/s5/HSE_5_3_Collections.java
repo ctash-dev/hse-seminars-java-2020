3c60051a1c5f9b25eca1000d7f9709b38ef98dc9
Kirill Kosolapov <ctash@yandex-team.ru>
2020-02-18 10:15:40 +0300

    Collections. Collection base methods.

--- src/ru/hse/s5/HSE_5_3_Collections.java
+++ src/ru/hse/s5/HSE_5_3_Collections.java
@@ -14,6 +14,13 @@ public class HSE_5_3_Collections {
         Collection<Integer> collection = list;
         list.addAll(List.of(1, 2, 3, 4, 5));
         Collections.sort(list); // how it works?
+        System.out.println(collection.size());
+        collection.add(6);
+        System.out.println(collection);
+        System.out.println(collection.contains(3));
+        System.out.println(collection.contains(7));
+        collection.remove(5);
+        System.out.println(collection);
     }
 
 

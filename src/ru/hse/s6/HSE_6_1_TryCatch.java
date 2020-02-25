2e5971a16faa1d34d1d95950501d75fb3069632b
Kirill Kosolapov <ctash@yandex-team.ru>
2020-02-25 10:10:28 +0300

    TryCatch. Handle inside

--- src/ru/hse/s6/HSE_6_1_TryCatch.java
+++ src/ru/hse/s6/HSE_6_1_TryCatch.java
@@ -17,7 +17,18 @@ public class HSE_6_1_TryCatch {
             throw e;
         }
 
+        handleExceptionInside();
     }
+
+    private static void handleExceptionInside() {
+        try {
+            throwChecked();
+        } catch (IOException e) {
+            // rethrow as MyNonCheckedException
+        }
+    }
+
+    private static final class MyNonCheckedException extends Exception {}
 
     private static void throwChecked() throws IOException {
 

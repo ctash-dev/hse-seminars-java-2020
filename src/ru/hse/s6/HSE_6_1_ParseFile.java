726d9fcd5a393153595669b0ad1ccf6cec381bb3
Kirill Kosolapov <ctash@yandex-team.ru>
2020-03-17 09:44:32 +0300

    Cleanup

--- src/ru/hse/s6/HSE_6_1_ParseFile.java
+++ src/ru/hse/s6/HSE_6_1_ParseFile.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -19,7 +20,9 @@ public class HSE_6_1_ParseFile {
 
 
     public static void main(String[] args) throws IOException {
-        File myStrings = new File("myStrings");
+        File myStrings = new File("myInstants");
+        myStrings.createNewFile();
+
         Duration diffDuration = getDurationBetweenMinAndMax(myStrings);
         System.out.println(diffDuration);
     }
@@ -30,12 +33,15 @@ public class HSE_6_1_ParseFile {
 
             final List<Instant> instants = lineStream.map(line -> getInstantFromString(line))
                     .collect(Collectors.toList());
+
+            final Instant min = Collections.min(instants);
+            final Instant max = Collections.max(instants);
+            return Duration.between(max, min);
         }
     }
 
     private static Instant getInstantFromString(String string) {
-        // todo
-        return null;
+        return Instant.parse(string);
     }
 
 

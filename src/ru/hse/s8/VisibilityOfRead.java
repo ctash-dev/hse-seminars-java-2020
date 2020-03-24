f3f8d72189cd1ece5eb3427be1c1758ba1519d67
Kirill Kosolapov <ctash@yandex-team.ru>
2020-03-24 09:35:53 +0300

    Threads. Inc for a counter. Fixed compilation.

--- src/ru/hse/s8/VisibilityOfRead.java
+++ src/ru/hse/s8/VisibilityOfRead.java
@@ -3,14 +3,18 @@ package ru.hse.s8;
 public class VisibilityOfRead {
 
     public static void main(String[] args) {
-        int counter = 0;
+        IntHolder intHolder = new IntHolder();
         for (int i = 0; i < 100; i++) {
             new Thread(() -> {
-                System.out.println("See counter as " + counter);
-                counter++;
+                System.out.println("See counter as " + intHolder.counter);
+                intHolder.counter++;
             }, "hseThread" + i);
 
         }
+    }
+
+    static class IntHolder {
+        int counter = 0;
     }
 
 }

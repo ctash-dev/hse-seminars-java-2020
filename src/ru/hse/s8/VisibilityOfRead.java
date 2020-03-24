f923ccfca84bc32efdbfb34f59e310a2340d8173
Kirill Kosolapov <ctash@yandex-team.ru>
2020-03-24 09:39:04 +0300

    Threads. Inc for a counter. Join. Kinda working

--- src/ru/hse/s8/VisibilityOfRead.java
+++ src/ru/hse/s8/VisibilityOfRead.java
@@ -2,14 +2,15 @@ package ru.hse.s8;
 
 public class VisibilityOfRead {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         IntHolder intHolder = new IntHolder();
         for (int i = 0; i < 100; i++) {
-            new Thread(() -> {
+            Thread thread = new Thread(() -> {
                 System.out.println("See counter as " + intHolder.counter);
                 intHolder.counter++;
             }, "hseThread" + i);
-
+            thread.start();
+            thread.join();
         }
     }
 
